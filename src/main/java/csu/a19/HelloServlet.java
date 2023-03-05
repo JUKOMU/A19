d4f9319169f7ef95ffbe7d83d70bc381d585dc69
JUKOMU <118241067+JUKOMU@users.noreply.github.com>
2023-03-05 20:52:20 +0800

    A19

--- src/main/java/csu/a19/HelloServlet.java
+++ src/main/java/csu/a19/HelloServlet.java
@@ -1,28 +1,29 @@
-package csu.a19;
-
-import java.io.*;
-
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
-
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class HelloServlet extends HttpServlet {
-    private String message;
-
-    public void init() {
-        message = "Hello World!";
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
-
-    public void destroy() {
-    }
-}
+//package csu.a19;
+//
+//import javax.servlet.*;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import java.io.IOException;
+//
+//@WebServlet(name = "helloServlet", value = "/hello-servlet")
+//public class HelloServlet extends HttpServlet {
+//    private String message;
+//
+//    public void init() {
+//        message = "Hello World!";
+//    }
+//
+//    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        response.setContentType("text/html");
+//
+//        // Hello
+//        PrintWriter out = response.getWriter();
+//        out.println("<html><body>");
+//        out.println("<h1>" + message + "</h1>");
+//        out.println("</body></html>");
+//    }
+//
+//    public void destroy() {
+//    }
+//}
---
+++ src/main/java/csu/a19/Register.java
@@ -0,0 +1,33 @@
+package csu.a19;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet("/register")
+public class Register implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
