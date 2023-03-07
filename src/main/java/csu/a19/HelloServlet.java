d6736db3caf5d8939e47f793bf0af5c2c4699d3c
JUKOMU <118241067+JUKOMU@users.noreply.github.com>
2023-03-08 00:07:38 +0800

    A19

--- src/main/java/csu/a19/HelloServlet.java
+++ src/main/java/csu/a19/HelloServlet.java
@@ -11,12 +11,10 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
     }
 }
 //
