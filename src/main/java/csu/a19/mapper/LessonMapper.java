35b9536afbfdfc1e28bc8a70d809bf2b3352359e
JUKOMU <118241067+JUKOMU@users.noreply.github.com>
2023-03-09 22:30:06 +0800

    修改课程数据的插入函数

--- src/main/java/csu/a19/mapper/LessonMapper.java
+++ src/main/java/csu/a19/mapper/LessonMapper.java
@@ -6,15 +6,6 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 public interface LessonMapper {
     /**
-     * 根据课程和老师查询课程对象
-     * @param classname
-     * @param teacher
-     * @return
-     */
-    @Select("select * from tb_classes where classname = #{classname} and teacher = #{teacher}")
-    Lesson select(@Param("classname") String classname, @Param("teacher") String teacher);
-
-    /**
      * 根据课程查询课程对象
      * @param classname
      * @return
@@ -30,6 +21,6 @@ public interface LessonMapper {
     @Select("select * from tb_classes where teacher = #{teacher}")
     Lesson selectByTeacher(String teacher);
 
-    @Insert("insert into tb_classes values(null,#{classname},#{teacher})")
+    @Insert("insert into tb_classes values(null,#{classname},#{teacher},#{CRY},#{pictures},#{date_and_time})")
     void add(Lesson lesson);
 }
