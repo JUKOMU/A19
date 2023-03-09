824212af4bcf35f99ee4932096dd95b6cfa74db5
JUKOMU <118241067+JUKOMU@users.noreply.github.com>
2023-03-09 22:22:20 +0800

    增加课程属性date_and_time和方法

--- src/main/java/csu/a19/lesson/Lesson.java
+++ src/main/java/csu/a19/lesson/Lesson.java
@@ -28,6 +28,11 @@ public class Lesson {
 
     public void setPictures(String[] pictures) { this.pictures = pictures; }
 
+    public String[] getDate_and_time() { return date_and_time; }
+
+    public void setDate_and_time(String[] date_and_time) { this.date_and_time = date_and_time; }
+
+
 
     @Override
     public String toString(){
